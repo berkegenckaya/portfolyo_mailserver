b4a1fee04838d401e9cb4eafb8e8e4fc483c852a
berkegenckaya <berkegenckaya@gmail.com>
2022-02-24 11:04:17 +0300

    cors

--- Server/index.js
+++ Server/index.js
@@ -1,50 +1,42 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import nodemailer from "nodemailer";
 
 dotenv.config();
 const app = express();
-app.use(cors({origin:"http://localhost:5500",credentials:true}));
+app.use(cors({ origin: "http://localhost:5500", credentials: false }));
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-    
-    host: 'smtp.office365.com',
-    Port: 587,
-    secure: false,
-    auth: {
-        user: process.env.MAIL,
-        pass:process.env.PASSWORD,
+  host: "smtp.office365.com",
+  Port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const mailinfo = req.body;
+  var mailOptions = {
+    from: process.env.MAIL,
+    to: "inthecasual@gmail.com",
+    subject: `${mailinfo.email} - ${mailinfo.name} `,
+    text: mailinfo.message,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-    
+  });
 });
-
-app.post("/" ,(req , res) => {
-    console.log(req.body)
-    const mailinfo = req.body;
-    var mailOptions = {
-        from:process.env.MAIL,
-        to: "inthecasual@gmail.com",
-        subject:  `${mailinfo.email} - ${mailinfo.name} `,
-        text: mailinfo.message,
-      };
-
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-
-})
-app.get("/", (req,res)=>{
-    res.send("selam")
-})
-app.listen(process.env.PORT || 5000 , () => {
+app.get("/", (req, res) => {
+  res.send("selam");
 });
-
-
-
-
+app.listen(process.env.PORT || 5000, () => {});
